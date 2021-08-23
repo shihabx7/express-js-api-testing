de22ee8979b1658220ffb2e2941d6dd65f46bc42
shihab khan <shihabx7@gmail.com>
2021-08-24 01:07:47 +0600

    response header, download, redirect

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ app.get("/",function(req,res){
 
     res.send("hello express js");
 });
+app.get("/download",function(req,res){
+
+   res.download("./uploads/gdn.png");
+});
+
+app.get("/red",function(req,res){
+
+    res.redirect("http://127.0.0.1:8000/404");
+ });
+
+ app.get("/404",function(req,res){
+
+    res.send("Page redirectt to 404");
+ });
+ app.get("/header",function(req,res){
+
+
+    res.append("name","shihab");
+    res.append("id","0123456");
+    res.append("city","Dhaka");
+    res.send("header data transfer test");
+    res.status(201).end();
+ })
+
 
 app.listen(8000,function(){
 console.log("Server is running");
